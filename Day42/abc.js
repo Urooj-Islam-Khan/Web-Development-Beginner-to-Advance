5e9db80b97169db4b50ce1c2a1a741a6b5699f87
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-04-19 21:25:26 +0500

    Add files via upload

---
+++ Day42/abc.js
@@ -0,0 +1,21 @@
+console.log("hello")
+
+let age = 3;
+let grace = 2;
+
+// arithmetic operator 
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)   //exponention
+console.log(age % grace)   //modulus
+
+if ((age + grace) > 18) {
+    console.log("you can drive a car")
+}
+
+else {
+    console.log("you can not drive a car")
+}
