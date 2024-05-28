52c56f8593d231b7ce0b1bab139d20dde5904783
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-05-28 11:49:28 +0500

    Add files via upload

---
+++ Day46/Index.js
@@ -0,0 +1,37 @@
+console.log("Stings")
+
+let name = "Urooj"
+let father = "Islam"
+
+console.log(name)
+
+// to access first character
+console.log(name[0])
+
+// to find the length 
+console.log(name.length)
+
+// concatenation
+console.log("I am " + name + " " + father)
+
+console.log(`I am ${name} ${father}`) //this is template literal
+
+// excape sequence 
+console.log("I am \"" + name + "\"")
+
+// upper case function 
+
+console.log(name.toUpperCase())
+
+// slice 
+
+console.log(name.slice(1, 4))
+
+a = "apple"
+console.log(a.replace("apple", "banana"))
+
+// to add string 
+
+console.log(father.concat(" khan"))
+
+// strings can never be change it is immutable 
