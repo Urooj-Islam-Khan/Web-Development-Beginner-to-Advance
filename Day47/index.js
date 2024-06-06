01eecc41eb9889ca576762a4a14fba06d3c3fda3
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-06-06 20:14:00 +0500

    Add files via upload

---
+++ Day47/index.js
@@ -0,0 +1,109 @@
+/* Create a business name generator by combining list of adjectives and shop name and another word 
+
+Adjectives:
+Crazy
+Amazing
+Fire
+
+Shop Name
+Engine
+Foods
+Garments
+
+
+Another Name:
+Bros
+Limited
+Hub
+*/
+
+
+
+
+
+console.log("Brand Name Generator")
+function Adjectives() {
+    let a = "Crazy"
+    let b = "Amazing"
+    let c = "Fire"
+    let rand = Math.floor(Math.random() * 3);
+    console.log(rand)
+
+    {
+        switch (rand) {
+            case 0:
+                return a;
+                break;
+
+            case 1:
+                return b;
+                break;
+
+            case 2:
+                return c;
+                break;
+
+            default:
+                console.log("no more value exist")
+                break;
+        }
+    }
+}
+
+function Shop_Name() {
+    let a = "Engine"
+    let b = "Foods"
+    let c = "Garments"
+    let rand = Math.floor(Math.random() * 3);
+    console.log(rand)
+
+    {
+        switch (rand) {
+            case 0:
+                return a;
+                break;
+
+            case 1:
+                return b;
+                break;
+
+            case 2:
+                return c;
+                break;
+
+            default:
+                console.log("no more value exist")
+                break;
+        }
+    }
+}
+
+function Other() {
+    let a = "Bros"
+    let b = "Limited"
+    let c = "Hub"
+    let rand = Math.floor(Math.random() * 3);
+    console.log(rand)
+    {
+        switch (rand) {
+            case 0:
+                return a;
+                break;
+
+            case 1:
+                return b;
+                break;
+
+            case 2:
+                return c;
+                break;
+
+            default:
+                console.log("no more value exist")
+                break;
+        }
+    }
+}
+
+
+console.log("Your brand name is" + Adjectives() + Shop_Name() + Other())
---
+++ Day47/tempCodeRunnerFile.js
@@ -0,0 +1,100 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Brand Name Generator")
+// function Adjectives() {
+//     let a = "Crazy"
+//     let b = "Amazing"
+//     let c = "Fire"
+//     let rand = Math.floor(Math.random() * 3);
+//     console.log(rand)
+
+//     {
+//         switch (rand) {
+//             case 0:
+//                 return a;
+//                 break;
+
+//             case 1:
+//                 return b;
+//                 break;
+
+//             case 2:
+//                 return c;
+//                 break;
+
+//             default:
+//                 console.log("no more value exist")
+//                 break;
+//         }
+//     }
+// }
+
+// function Shop_Name() {
+//     let a = "Engine"
+//     let b = "Foods"
+//     let c = "Garments"
+//     let rand = Math.floor(Math.random() * 3);
+//     console.log(rand)
+
+//     {
+//         switch (rand) {
+//             case 0:
+//                 return a;
+//                 break;
+
+//             case 1:
+//                 return b;
+//                 break;
+
+//             case 2:
+//                 return c;
+//                 break;
+
+//             default:
+//                 console.log("no more value exist")
+//                 break;
+//         }
+//     }
+// }
+
+// function Other() {
+//     let a = "Bros"
+//     let b = "Limited"
+//     let c = "Hub"
+//     let rand = Math.floor(Math.random() * 3);
+//     console.log(rand)
+//     {
+//         switch (rand) {
+//             case 0:
+//                 return a;
+//                 break;
+
+//             case 1:
+//                 return b;
+//                 break;
+
+//             case 2:
+//                 return c;
+//                 break;
+
+//             default:
+//                 console.log("no more value exist")
+//                 break;
+//         }
+//     }
+// }
+
+
+// console.log("Your brand name is" + Adjectives() + Shop_Name() + Other())
