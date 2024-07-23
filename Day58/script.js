0e2bf703ade0553b3abf6598bb371aec01c5bfd1
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-07-23 16:12:08 +0500

    Update script.js

--- Day58/script.js
+++ Day58/script.js
@@ -1,13 +1,55 @@
+let child = document.querySelector(".child");
+child.addEventListener("click", (e) => {
+    // stopPropagation use islie krte k event bubbling ko stop krein 
+    // event bubbling ka matlab k ap kisi pr event listen kroge to uske parent ka b event listener activate hojae ga for example ap hyderabad me rehte ho to technically ap pakistan me b rehte ho aese 
+    e.stopPropagation();
+    child.style.backgroundColor = "red"
+    child.style.color = "white"
+})
 
+document.querySelector(".childContainer").addEventListener("click",
+    (e) => {
+        e.stopPropagation();
+        alert("child container was clicked")
+    })
 
-const callback = (arg) => {
-    console.log(arg)
+document.querySelector(".container").addEventListener("click",
+    (e) => {
+        e.stopPropagation();
+        alert("Container was clicked")
+    })
+
+function getrandomcolor() {
+    let col1 = Math.ceil(0 + Math.random() * 255);
+    let col2 = Math.ceil(0 + Math.random() * 255);
+    let col3 = Math.ceil(0 + Math.random() * 255);
+    return `rgb(${col1}, ${col2}, ${col3})`
 }
-const loadScript = (src, callback) => {
-    let sc = document.createElement("script");
-    sc.src = src;
-    sc.onload = callback("Urooj");
-    document.head.append(sc);
+function getrandomborder() {
+    let col1 = Math.ceil(0 + Math.random() * 255);
+    let col2 = Math.ceil(0 + Math.random() * 255);
+    let col3 = Math.ceil(0 + Math.random() * 255);
+    return `3px solid rgb(${col2}, ${col3}, ${col1})`
 }
+// to repeat anything in one second use this 
+let a = setInterval(() => {
+    document.querySelector(".childContainer").style.background = getrandomcolor();
+    document.querySelector(".childContainer").style.border = getrandomborder()
+}, 1000);
 
-console.log(loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback))
+console.log(a);
+
+// to stop the intervcal
+clearInterval(a)
+
+let b = setTimeout(() => {
+    document.querySelector(".container").style.background = getrandomcolor();
+    document.querySelector(".container").style.border = getrandomborder()
+}, 1000);
+
+clearTimeout(b)
+
+
+
+
+
